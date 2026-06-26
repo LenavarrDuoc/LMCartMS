24b85727a98c0c34b127b1c19e72f13cca063b63
LenavarrDuoc <leo.navarro@duocuc.cl>
2026-06-25 23:50:53 -0400

    Implementación de JaCoCo y generación de reporte.

--- src/test/java/cl/duoc/lmcartms/LmCartMsApplicationTests.java
+++ src/test/java/cl/duoc/lmcartms/LmCartMsApplicationTests.java
@@ -2,8 +2,18 @@ package cl.duoc.lmcartms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "eureka.client.enabled=false",
+        "spring.cloud.discovery.enabled=false",
+        "spring.main.allow-bean-definition-overriding=true"
+})
+//@EnableAutoConfiguration(exclude = {
+//        DataSourceAutoConfiguration.class,
+//        HibernateJpaAutoConfiguration.class
+//})
+@ActiveProfiles("test")
 class LmCartMsApplicationTests {
 
     @Test
